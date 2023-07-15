26f36fd1920e186a014c4108cb41a067b485ea0a
Cocozo <kjh062627@gmail.com>
2023-07-15 19:08:27 +0900

    add simple bra-ket expr converter

--- qiskit_class_converter/converters/braket_notation_to_matrix.py
+++ qiskit_class_converter/converters/braket_notation_to_matrix.py
@@ -5,6 +5,7 @@ from sympy.parsing.sympy_parser import parse_expr
 from sympy import expand
 from sympy.parsing.latex import parse_latex
 from sympy.physics.quantum import Bra,Ket
+from sympy import symbols, Eq, solve, sympify
 import re
 
 from qiskit_class_converter.converters.base import BaseConverter
@@ -18,22 +19,28 @@ class BraketNotationToMatrixConverter(BaseConverter):
    
     def parse_braket(self):
         # result must sympy 
+        #detect bra, ket, sign, cant detect sign but plus, minus can 
         regex = re.compile("(?=(?P<bra>[<][01]+[|]))|(?=(?P<ket>[|][01]+[>]))|(?=(?P<symbol>[+-]))")
         extr = re.compile("[01]+")
-        exprs = regex.findall("|00>+|11>")
-        symbols = []
+        exprs = regex.findall("(sqrt(2)/2)*|00> + (sqrt(2)/2)*|11>")
+        symbol = []
+        result = None
         for expr in exprs :
             #케이스에 따른 expr 처리
+            #ver.1 simple expr
+            #Bra
             if expr[0] != "":
                 val = extr.search(expr[0]).group()
                 # val = int(val)
-                symbols.append(Bra(val))
+                symbol.append(Bra(val))
+            #Ket
             elif expr[1] != "":
                 val = extr.search(expr[1]).group()
                 # val = int(val)
-                symbols.append(Ket(val))
-            else : continue # making process
-        print(symbols)
+                symbol.append(Ket(val))
+
+        result = parse_expr("(sqrt(2)/2)*x + (sqrt(2)/2)*y", evaluate=False, transformations='all', local_dict={'x': symbol[0], 'y': symbol[1]})
+        print(result)
 
     
     def actual_convert_action(self):
