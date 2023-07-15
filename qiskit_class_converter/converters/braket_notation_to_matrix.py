75f66be426357b9b84ee36d991f721b73f7328a3
Cocozo <kjh062627@gmail.com>
2023-07-15 20:13:32 +0900

    add multiple bra-ket parse system

--- qiskit_class_converter/converters/braket_notation_to_matrix.py
+++ qiskit_class_converter/converters/braket_notation_to_matrix.py
@@ -20,28 +20,38 @@ class BraketNotationToMatrixConverter(BaseConverter):
     def parse_braket(self):
         # result must sympy 
         #detect bra, ket, sign, cant detect sign but plus, minus can 
-        regex = re.compile("(?=(?P<bra>[<][01]+[|]))|(?=(?P<ket>[|][01]+[>]))|(?=(?P<symbol>[+-]))")
+        regex_pattern = "(?=(?P<bra>[<][01]+[|]))|(?=(?P<ket>[|][01]+[>]))|(?=(?P<symbol>[+-]))"
         extr = re.compile("[01]+")
-        exprs = regex.findall("(sqrt(2)/2)*|00> + (sqrt(2)/2)*|11>")
-        symbol = []
-        result = None
-        for expr in exprs :
+        
+        expr_str = "(sqrt(2)/2)*|00> + (sqrt(2)/2)*|11>"
+        expr_symbols = re.findall(regex_pattern, expr_str)
+        dict_num = 97
+        local_dict_str = {}
+        local_dict ={}
+        for symbol in expr_symbols :
             #케이스에 따른 expr 처리
             #ver.1 simple expr
             #Bra
-            if expr[0] != "":
-                val = extr.search(expr[0]).group()
+            if symbol[0] != "":
+                val = extr.search(symbol[0]).group()
                 # val = int(val)
-                symbol.append(Bra(val))
+                local_dict_str[chr(dict_num)] = symbol[0]
+                local_dict[chr(dict_num)] = (Bra(val))
+                dict_num += 1
             #Ket
-            elif expr[1] != "":
-                val = extr.search(expr[1]).group()
+            elif symbol[1] != "":
+                val = extr.search(symbol[1]).group()
                 # val = int(val)
-                symbol.append(Ket(val))
+                local_dict_str[chr(dict_num)] = symbol[1]
+                local_dict[chr(dict_num)] = (Ket(val))
+                dict_num += 1
+            #bra-ket
 
-        result = parse_expr("(sqrt(2)/2)*x + (sqrt(2)/2)*y", evaluate=False, transformations='all', local_dict={'x': symbol[0], 'y': symbol[1]})
-        print(result)
-
+        for key in local_dict.keys():
+            expr_str = expr_str.replace(local_dict_str[key], key,1)
+        expr = parse_expr(expr_str, evaluate=False, transformations='all', local_dict=local_dict)
+        print(expr)
+        return expr
     
     def actual_convert_action(self):
         # self.logger.debug("bra-ket notation to matrix")
