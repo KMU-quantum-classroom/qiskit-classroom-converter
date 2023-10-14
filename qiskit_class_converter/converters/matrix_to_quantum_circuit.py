6bb67e2193d1e1187895533b346968f14a070445
Cocozo <kjh062627@gmail.com>
2023-10-14 19:19:46 +0900

    nothing but for backup my work

--- qiskit_class_converter/converters/matrix_to_quantum_circuit.py
+++ qiskit_class_converter/converters/matrix_to_quantum_circuit.py
@@ -35,50 +35,80 @@ class MatrixToQuantumCircuitConverter(BaseConverter):
         """
         load matrix preset
         """
-
         mat_1bit = None
-        mat_2bit = None
         with open('./qiskit_class_converter/converters/preset/matrices_1bit.json',
                    encoding='utf-8') as file:
             mat_1bit = json.load(file)
-
         with open('./qiskit_class_converter/converters/preset/matrices_2bit.json',
                    encoding='utf-8') as file:
             mat_2bit = json.load(file)
 
         return mat_1bit, mat_2bit
 
-    # 재귀 알고리즘 or 반복문 사용 -> high load
+
     def make_circuit(self, target, dimension) :
         """
         make_circuit from assumed circuit list -> todo
         """
         return 0
 
+    def matching_circuit(self, target, qubits, result):
+
+        # 실행시간 -> big O(preset_time^n)
+        mat_1bit, mat_2bit = self.load_matrix_preset()
+        size_1bit = len(mat_1bit["matrices"])
+        size_2bit = len(mat_2bit["matrices"])
+        
+        
+        for i in range(pow((size_1bit+size_2bit), qubits)):
+            product = None
+                
+        return result
+
+    def combine_circuit(self, target, size):
+        """
+        combine possible circuit list -todo
+        """
+        result = []
+
+        #checking qubit size
+        qubits = 0
+        while size > 1:
+            size /= 2
+            qubits += 1
+
+        self.logger.debug(f"num of qubits : {qubits}")
+
+        # making kroneker product
+        # it should be {q_n, q_n-1, q_n-2 ... q_0}
+
+        # maybe it's ok to make brute froce search function
+        
+        return result
+
     # matrix 탐색 -> tree 형으로 완전탐색을 기본으로 감.
     def measure_circuit(self, target) :
         """
         assuming circuit -> todo
         """
-        #get matrix dimension -> checking dimension is too large
-        target_dimention = None
+
+        #get matrix size -> checking size is too large
+        target_size = None
         if isinstance(target, List) :
-            target_dimention = int(len(target))
-        elif isinstance(target, np.ndarray):
-            target_dimention = target.ndim
+            target = np.asarray(target)
 
-        self.logger.debug(target_dimention)
+        target_size = int(np.sqrt(target.size))
 
-        #checking dimension
-        #2, 4, 8 dimension is only accepted dimension
-        if target_dimention > 8 :
+        self.logger.debug(f"array size : {target_size}")
+
+        #checking size
+        #2, 4, 8 size is only accepted dimension
+        if target_size > 8 :
             raise ValueError("dimension is too large please input ")
-        if target_dimention % 2 != 0 and target_dimention == 6:
-            raise ValueError("invalid dimension size")
-        
-        mat_1bit, mat_2bit = self.load_matrix_preset()
+        if target_size % 2 != 0 and target_size == 6:
+            raise ValueError("invalid size")
 
-        
+        result = self.combine_circuit(target, target_size)
 
         return 0
 
