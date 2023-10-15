457bb9931729a17080feeb543c6672a741ca6e8a
Cocozo <kjh062627@gmail.com>
2023-10-15 17:46:37 +0900

    add circuit estimate system(unstable work)

--- qiskit_class_converter/converters/matrix_to_quantum_circuit.py
+++ qiskit_class_converter/converters/matrix_to_quantum_circuit.py
@@ -46,31 +46,92 @@ class MatrixToQuantumCircuitConverter(BaseConverter):
         return mat_1bit, mat_2bit
 
 
-    def make_circuit(self, target, dimension) :
+    def make_circuit(self, target, size) :
         """
         make_circuit from assumed circuit list -> todo
         """
         return 0
 
-    def matching_circuit(self, target, qubits, result):
+
+    def kroneker_product(self, gates):
+        """
+        calculte kroneker product given gates
+        """
+        result = None
+
+        for i, gate in enumerate(gates) :
+            temp = np.asarray(gate["matrix"])
+            if i == 0 :
+                result = temp
+            else :
+                result = np.kron(temp, result)
+
+        return result
+
+    def matching_circuit(self, target, qubits, layer = 0, result = None):
+        """
+        main circuit estimating sequence
+        """
+
+        if result == None :
+            result = []
 
         # 실행시간 -> big O(preset_time^n)
         mat_1bit, mat_2bit = self.load_matrix_preset()
         size_1bit = len(mat_1bit["matrices"])
-        size_2bit = len(mat_2bit["matrices"])
-        
-        
-        for i in range(pow((size_1bit+size_2bit), qubits)):
-            product = None
-                
-        return result
+
+        for i in range(pow(size_1bit, qubits)):
+            gates = []
+            for q in range(qubits) :
+                if q == 0:
+                    gates.append(mat_1bit["matrices"][i%size_1bit])
+                else:
+                    gates.append(mat_1bit["matrices"][i//pow(size_1bit, qubits)])
+
+            #gate kroneker product
+            kroneker = self.kroneker_product(gates)
+
+            #make layer
+            layer = {
+                "gates" : gates,
+                "kroneker" : kroneker
+            }
+
+            #append layer to result
+            result.append(layer)
+
+            estim = None
+            #calculate layers
+            for i, layer in enumerate(result):
+                if i == 0:
+                    estim = layer["kroneker"]
+                else :
+                    estim = layer["kroneker"] * estim
+
+            #checking comparison target and estimated matrix
+            if np.array_equal(target, estim):
+                return result
+
+            #layer limit is 5
+            if layer == 5:
+                continue
+
+            #get next layer -> Depth first
+            result = self.matching_circuit(target, qubits, result, layer+1)
+
+            if isinstance(result, list):
+                return result
+
+            self.logger.debug(f"layer{layer} : change layer gate")
+            result.pop()
+
+        return -1
+
 
     def combine_circuit(self, target, size):
         """
         combine possible circuit list -todo
         """
-        result = []
-
         #checking qubit size
         qubits = 0
         while size > 1:
@@ -81,8 +142,9 @@ class MatrixToQuantumCircuitConverter(BaseConverter):
 
         # making kroneker product
         # it should be {q_n, q_n-1, q_n-2 ... q_0}
-
+        # todo -> make matching_circuit function
         # maybe it's ok to make brute froce search function
+        result = self.matching_circuit(target, qubits)
         
         return result
 
@@ -102,15 +164,15 @@ class MatrixToQuantumCircuitConverter(BaseConverter):
         self.logger.debug(f"array size : {target_size}")
 
         #checking size
-        #2, 4, 8 size is only accepted dimension
+        #2, 4, 8 size is only accepted size
         if target_size > 8 :
-            raise ValueError("dimension is too large please input ")
+            raise ValueError("size is too large please input ")
         if target_size % 2 != 0 and target_size == 6:
             raise ValueError("invalid size")
 
         result = self.combine_circuit(target, target_size)
 
-        return 0
+        return result
 
     def actual_convert_action(self):
         self.logger.debug("matrix to quantum circuit")
